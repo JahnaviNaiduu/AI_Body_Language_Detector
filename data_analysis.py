4ee18882daff87d2ef5d2e475c99f79faac7cca0
SrihariniRajasekar <sriharinisekar06@gmail.com>
2025-06-13 20:53:35 +0530

    data_analysis.py
    Till model deployment

--- data_analysis.py
+++ data_analysis.py
@@ -39,3 +39,41 @@ plt.grid(True)
 plt.show()
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1234)
+
+#Model Building
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression,RidgeClassifier
+from sklearn.ensemble import RandomForestClassifier,GradientBoostingClassifier
+from sklearn.impute import SimpleImputer
+
+pipelines={
+    'lr':make_pipeline(SimpleImputer(strategy='mean'),StandardScaler(),LogisticRegression()),
+    'rc':make_pipeline(SimpleImputer(strategy='mean'),StandardScaler(),RidgeClassifier()),
+    'rf':make_pipeline(SimpleImputer(strategy='mean'),StandardScaler(),RandomForestClassifier()),
+    'gb':make_pipeline(SimpleImputer(strategy='mean'),StandardScaler(),GradientBoostingClassifier()),
+}
+
+fit_models={}
+for algo,pipeline in pipelines.items():
+    model=pipeline.fit(X_train,y_train)
+    fit_models[algo]=model
+
+#ModelDeployment
+
+from sklearn.metrics import accuracy_score
+import pickle
+
+for algo,model in fit_models.items():
+    yhat=model.predict(X_test)
+    print(algo,accuracy_score(y_test,yhat))
+
+
+import os
+# Ensure directory exists
+os.makedirs(r'C:\Users\91944\Documents\AI_Body_Language_Detector\Model', exist_ok=True)
+# Save the model
+with open(r'C:\Users\91944\Documents\AI_Body_Language_Detector\Model\body_language.pkl', 'wb') as f:
+    pickle.dump(fit_models['lr'], f)
+with open(r'C:\Users\91944\Documents\AI_Body_Language_Detector\Model\body_language.pkl', 'rb') as f:
+    model=pickle.load(f)
